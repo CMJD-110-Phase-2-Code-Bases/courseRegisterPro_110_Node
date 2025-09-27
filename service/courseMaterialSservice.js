56ef5b88aa9bae2f83a2db8577759669865a3da6
ThanuraSilva <thanurasilva1989@gmail.com>
2025-09-27 10:46:25 +0530

    Set up courseMaterial Service

---
+++ service/courseMaterialSservice.js
@@ -0,0 +1,23 @@
+
+//getAll
+
+async function getAllCourseMaterials(){
+
+}
+
+// save
+
+async function addCourseMaterial(){
+
+}
+//delete
+async function deleteCourseMaterial(){
+
+
+}
+//update
+async function updateCoureMaterial(){
+
+}
+
+module.exports = { getAllCourseMaterials, addCourseMaterial, updateCoureMaterial,deleteCourseMaterial }
