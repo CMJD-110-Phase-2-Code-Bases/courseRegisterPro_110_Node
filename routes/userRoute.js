43640e4d1af9e6abf1a35f07b6ec516cd7cfc9bb
ThanuraSilva <thanurasilva1989@gmail.com>
2025-09-27 14:21:58 +0530

    Create Signup function

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,9 +2,12 @@ const authUrl = "/auth"
 const express = require('express')
 const router = express.Router()
 const userService = require("../service/userService")
-const CourseMaterial = require("../model/userModel")
+const User = require("../model/userModel")
 const { v4: uuidv4 } = require("uuid")
+const jwt = require("jsonwebtoken")
+require('dotenv').config()
 
+const jwtSecret = process.env.JWT_SECRET
 
 //Sign in
 router.post(`${authUrl}/signin`,async (req,res)=>{
@@ -27,10 +30,9 @@ router.post(`${authUrl}/signup`,async (req,res)=>{
         res.status(400).json({"error":"Missing required input fileds"})
     }
     try{
-    
-
-
-
+       const createdUser = userService.addUser(req.body);
+       const token = jwt.sign({ userId: user.email},jwtSecret, { expiresIn: '1h'})
+       res.status(201).json({token: token})
 
     }catch(err){
         console.err("Loging error",err);
--- service/userService.js
+++ service/userService.js
@@ -1,1 +1,24 @@
 const User = require("../model/userModel")
+const bcrypt = require("bcryptjs")
+const { v4: uuid4 } = require('uuid')
+
+async function addUser(user){
+    try{
+        const hashPassword = await bcrypt.hash(user.password, 10 )
+        const newUser = new User({
+            userId: uuid4(),
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email:user.email,
+            password : hashPassword,
+            role : user.role
+        })
+
+        return newUser.save();
+
+    }catch (err){
+        console.error("User create failed",err)
+        throw err;
+    }
+}
+module.exports = { addUser }
