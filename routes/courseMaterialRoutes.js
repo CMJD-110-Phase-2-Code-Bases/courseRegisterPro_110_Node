fb2d6d03edcea23cdfcba5d0cc59a7905456b520
ThanuraSilva <thanurasilva1989@gmail.com>
2025-09-27 11:42:49 +0530

    Add multer

--- routes/courseMaterialRoutes.js
+++ routes/courseMaterialRoutes.js
@@ -1,18 +1,26 @@
 const courseMaterialURL = "/material"
 const express = require('express')
 const router = express.Router()
+const courseMaterialService = require("../service/courseMaterialService")
 
 
 
 // get CM
-router.get(courseMaterialURL,(req,res)=>{
-   res.send("CM Routes")
+router.get(courseMaterialURL, async (req,res)=>{
+   try{
+       const allMaterials = await courseMaterialService.getAllCourseMaterials()
+       return res.json(allMaterials)
+   }catch(err){
+        console.error("Error fetching materials ",err)
+        return res.status(500).json({error:"Failed to fetch data"})
+   }
 })
 
-// // save CM
-// router.post(courseMaterialURL, courseMat,(req,res)=>{
+// save CM
+router.post(courseMaterialURL, courseMat,(req,res)=>{
     
-// })
+    
+})
 // // update CM
 // router.patch(`${courseMaterialURL}/:materialId`,courseMat,(req,res)=>{
     
